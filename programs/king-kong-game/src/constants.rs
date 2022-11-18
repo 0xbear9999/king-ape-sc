dcc37e7fa2012e78dcc70dc057f847cc63b2b4a4
0xbear9999 <supernova1111a@outlook.com>
2022-11-18 06:57:14 +0000

    currently deployed smart contract for king-kong game

---
+++ programs/king-kong-game/src/constants.rs
@@ -0,0 +1,17 @@
+pub const GLOBAL_AUTHORITY_SEED: &str = "global-authority-v1";
+pub const USER_DATA_SEED: &str = "user-info-v1";
+pub const ESCROW_VAULT_SEED: &str = "escrow-vault";
+
+pub const BANANA_TOKEN_MINT_PUBKEY: &str = "AsACVnuMa5jpmfp3BjArmb2qWg5A6HBkuXePwT37RrLY";
+pub const GAME_VAULT_SEED: &str = "game-vault";
+pub const GAME_CONFIGVAULT_SEED: &str = "game-config-vault";
+pub const RANDOM_SEED: &str = "set-random-numbers";
+
+pub const COLLECTION_ADDRESS: &str = "G42V1DfQKKHrxxfdjDrRphPStZx5Jqu2JwShfN3WoKmK";
+
+pub const DAILY_REWARD_DIST_WALLET: &str = "9tHZYzwQj1Msjv5pQbg1r4EHNU3wzmqB4mwzqEqnf1GC";
+pub const GOLD_CHEST_WALLET: &str = "9D8rpyCQ7t58SnePbMmm4sZHRW5QsqPHCT5B6GnVvoBo";
+pub const BANANA_TREASURY_WALLET: &str = "ARmKoQrb53Tn5vDApZdaZSUKqDqkR22Tpd6TqTsHA8cw";
+
+pub const NFT_DATA_SEED: &str = "nft-data";
+pub const DEFAULT_PUBKEY: &str = "11111111111111111111111111111111";
